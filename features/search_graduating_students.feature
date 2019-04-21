e5c2f0b19ae511567216289af09edc4c76d96126
Kingdon Barrett <kingdon.b@nd.edu>
2019-04-21 13:13:11 -0400

    feat(cucumber): pending scenario

---
+++ features/search_graduating_students.feature
@@ -0,0 +1,8 @@
+Feature: Search Graduating Students
+  Background:
+    Given an environment variable provides an S3 URL
+
+Scenario: 
+  Given the S3 URL has a list of student names
+  When the user types "Graduating Stud" into the search box
+  Then the interface presents "Graduating Student" as the first autocomplete option
---
+++ features/step_definitions/search_graduating_students_steps.rb
@@ -0,0 +1,15 @@
+Given("an environment variable provides an S3 URL") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("the S3 URL has a list of student names") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the user types {string} into the search box") do |partial_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the interface presents {string} as the first autocomplete option") do |full_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
